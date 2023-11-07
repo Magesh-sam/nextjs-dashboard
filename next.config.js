73c77ad0ac330689a9116cbe92a89f7864dffc43
mageshkannan <mageshkannanam@gmail.com>
2023-11-07 11:16:02 +0530

    updated next config for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "next-js-dashboard-tut.vercel.app",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
